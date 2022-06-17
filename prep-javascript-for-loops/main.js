946580aae0051d39dd68a40c60b9986c729e64f5
mbarber0707 <michaelbarber0707@gmail.com>
2022-06-17 06:05:13 +0000

    completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,14 @@
+// create your loops here.
+// for (let v = 0; v <= 9; v++) {
+//   console.log(v);
+// }
+
+// for (let v = 0; v < 20; v += 2) {
+//   console.log(v);
+// }
+
+for (let v = 100; v > 0; v--) {
+  console.log(v);
+}
+// v = 100
+// v = 99
